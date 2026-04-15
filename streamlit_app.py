8fc1bd49d8743df66c87d4ee4d09861fef1f8aa1
EBONAMOUR <ethanbonamour@kubrickgroup.com>
2026-04-15 14:46:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredient_list:
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Infomation')
-        smothiefroot_response = requests.Get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)  
+        smothiefroot_response = requests.Get("https://my.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
